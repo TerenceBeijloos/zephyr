04bf75169bad28b6be11389a478197a425f31ba3
TerenceBeijloos <tgmbeijl@avans.nl>
2021-11-04 14:06:14 +0100

    Resolved cmake build errors involving the gpio node

---
+++ soc/arm/DA1469x/soc.h
@@ -0,0 +1,15 @@
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#error "do i get here?"
+#define __NVIC_PRIO_BITS        NUM_IRQ_PRIO_BITS
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SOC__H_ */
