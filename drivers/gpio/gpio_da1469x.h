8a455d43e60c8e8b72afa82d0696bff9d81a057f
TerenceBeijloos <tgmbeijl@avans.nl>
2021-11-05 16:51:26 +0100

    Started adding gpio source, ERROR: No SOURCES given to Zephyr library: drivers__gpio

---
+++ drivers/gpio/gpio_da1469x.h
@@ -0,0 +1,12 @@
+#ifndef ZEPHYR_DRIVERS_GPIO_GPIO_DA1469X_H_
+#define ZEPHYR_DRIVERS_GPIO_GPIO_DA1469X_H_
+
+#include <drivers/gpio.h>
+
+
+struct gpio_da1469x_config {
+	struct gpio_driver_config common;
+    //TODO
+};
+
+#endif /* ZEPHYR_DRIVERS_GPIO_GPIO_DA1469X_H_ */
