c9ec56a9cf81e4506cd843ac7990ef3a3adce907
TerenceBeijloos <42936949+TerenceBeijloos@users.noreply.github.com>
2022-01-05 11:30:53 +0100

    Working hello world

--- drivers/serial/uart_da1469x.c
+++ drivers/serial/uart_da1469x.c
@@ -5,13 +5,13 @@
 
 struct uart_da1469x_config
 {
-    struct uart_device_config uart_config;
+    struct uart_device_config uart_dev_config;
 	uart_id id;
 };
 
 struct uart_da1469x_data
 {
-    struct uart_config uart_current_config;
+    struct uart_config uart_config;
 };
 
 #define DEV_CFG(_dev) ((struct uart_da1469x_config *const)(_dev)->config)
@@ -44,7 +44,7 @@ static int uart_da1469x_init(const struct device *dev)
 {
     ARG_UNUSED(dev);
 
-	uart_da1469x_configure(dev, &(DEV_CFG(dev)->uart_config));
+	uart_da1469x_configure(dev, &(DEV_DATA(dev)->uart_config));
 
 
 	return 0;
@@ -56,22 +56,76 @@ static const struct uart_driver_api uart_da1469x_driver = {
 	.err_check = NULL,
 };
 
-#define DA1469X_UART_DEVICE_INIT(inst)                                      \
-	static const struct uart_da1469x_config uart_da1469x_cfg_##inst = {     \
-            .uart_config = (uint8_t *)DT_INST_REG_ADDR(index),              \
-			.id = DT_INST_PROP(inst, uart_id)								\
-	};                                                                      \
-									                                        \
-    static struct uart_da1469x_data uart_da1469x_data_##inst;	            \
-									                                        \
-	DEVICE_DT_INST_DEFINE(inst,					                            \
-			    uart_da1469x_init,                                          \
-			    NULL,					                                    \
-			    &uart_da1469x_data_##inst,                                  \
-			    &uart_da1469x_cfg_##inst,                                   \
-			    PRE_KERNEL_1,                                               \
-			    CONFIG_SERIAL_INIT_PRIORITY,	                            \
+#define DA1469X_UART_DEVICE_INIT(inst)                                      					\
+	static const struct uart_da1469x_config uart_da1469x_cfg_##inst = {     					\
+			.uart_dev_config = {							       								\
+				.base =							       											\
+		   		 		(uint8_t *)DT_INST_REG_ADDR(inst), 										\
+						},																		\
+			.id = DT_INST_PROP(inst, uart_id)-1													\
+	};                                                                      					\
+									                                        					\
+    static struct uart_da1469x_data uart_da1469x_data_##inst = {	        					\
+		.uart_config = {						       											\
+			.baudrate = DT_INST_PROP(inst, current_speed),										\
+			.parity = DT_ENUM_IDX_OR(DT_DRV_INST(index), parity, UART_CFG_PARITY_NONE),			\							
+			.stop_bits = DT_INST_PROP(inst, stop_bits),											\
+			.data_bits = DT_INST_PROP(inst, data_bits),											\
+			.flow_ctrl = DT_INST_PROP(inst, hw_flow_control),									\
+			}								       												\
+	};									       													\
+									                                        					\
+	DEVICE_DT_INST_DEFINE(inst,					                            					\
+			    uart_da1469x_init,                                          					\
+			    NULL,					                                    					\
+			    &uart_da1469x_data_##inst,                                  					\
+			    &uart_da1469x_cfg_##inst,                                   					\
+			    PRE_KERNEL_1,                                               					\
+			    CONFIG_SERIAL_INIT_PRIORITY,	                            					\
 			    &uart_da1469x_driver);
 
-DT_INST_FOREACH_STATUS_OKAY(DA1469X_GPIO_DEVICE_INIT);
+DT_INST_FOREACH_STATUS_OKAY(DA1469X_UART_DEVICE_INIT);
 
+
+// #define DA1469X_UART_DEVICE_INIT(inst)						       \
+// static const struct uart_da1469x_config uart_da1469x_cfg_port_##inst = {	       \
+// 	.uart_dev_config = {							       \
+// 		.base =							       \
+// 		    (uint8_t *)DT_REG_ADDR(DT_NODELABEL(uart##idx)), \
+// 	},								       \
+// 	},								       \
+// 									       \
+// 	.pins = {							       \
+// 		.tx = DT_PROP(DT_NODELABEL(uart##idx), tx_pin),	       \
+// 		.rx = DT_PROP(DT_NODELABEL(uart##idx), rx_pin),	       \
+// 		IF_ENABLED(						       \
+// 			DT_PROP(DT_NODELABEL(uart##idx), hw_flow_control),  \
+// 			(.rts = DT_PROP(DT_NODELABEL(uart##idx), rts_pin),  \
+// 			.cts = DT_PROP(DT_NODELABEL(uart##idx), cts_pin),   \
+// 			))						       \
+// 	},								       \
+// 											   \
+// 	.clock_subsys = (clock_control_subsys_t)DT_CLOCKS_CELL(DT_NODELABEL(uart##idx), offset), \
+// 	.irq_source = DT_IRQN(DT_NODELABEL(uart##idx))			       \
+// };									       \
+// 									       \
+// static struct uart_esp32_data uart_esp32_data_##idx = {			       \
+// 	.uart_config = {						       \
+// 		.baudrate = DT_PROP(DT_NODELABEL(uart##idx), current_speed),\
+// 		.parity = UART_CFG_PARITY_NONE,				       \
+// 		.stop_bits = UART_CFG_STOP_BITS_1,			       \
+// 		.data_bits = UART_CFG_DATA_BITS_8,			       \
+// 		.flow_ctrl = IS_ENABLED(				       \
+// 			DT_PROP(DT_NODELABEL(uart##idx), hw_flow_control)) ?\
+// 			UART_CFG_FLOW_CTRL_RTS_CTS : UART_CFG_FLOW_CTRL_NONE   \
+// 	}								       \
+// };									       \
+// 									       \
+// DEVICE_DT_DEFINE(DT_NODELABEL(uart##idx),				       \
+// 		    &uart_esp32_init,					       \
+// 		    NULL,				       \
+// 		    &uart_esp32_data_##idx,				       \
+// 		    &uart_esp32_cfg_port_##idx,				       \
+// 		    PRE_KERNEL_1,					       \
+// 		    CONFIG_SERIAL_INIT_PRIORITY,			       \
+// 		    &uart_esp32_api);
